df101a74543377e5003a6b7da091483d88a324a2
axaysushir <akshay.sushir99@gmail.com>
2021-10-05 13:13:51 +0530

    Post request in go

--- request.go
+++ request.go
@@ -46,5 +46,8 @@ func main() {
 	}
 	sb := string(body)
 	log.Printf(sb)
-	// 2021/10/05 13:11:33 {"args":{},"data":{"email":"james@google.com","name":"James"},"files":{},"form":{},"headers":{"x-forwarded-proto":"https","x-forwarded-port":"443","host":"postman-echo.com","x-amzn-trace-id":"Root=1-615c01ad-6734c6eb54e268eb74643092","content-length":"43","content-type":"application/json","accept-encoding":"gzip","user-agent":"Go-http-client/2.0"},"json":{"email":"james@google.com","name":"James"},"url":"https://postman-echo.com/post"}
+	// 2021/10/05 13:11:33 {"args":{},"data":{"email":"james@google.com","name":"James"},"files":{},
+	// "form":{},"headers":{"x-forwarded-proto":"https","x-forwarded-port":"443","host":"postman-echo.com",
+	// "x-amzn-trace-id":"Root=1-615c01ad-6734c6eb54e268eb74643092","content-length":"43","content-type":"application/json",
+	// "accept-encoding":"gzip","user-agent":"Go-http-client/2.0"},"json":{"email":"james@google.com","name":"James"},"url":"https://postman-echo.com/post"}
 }
