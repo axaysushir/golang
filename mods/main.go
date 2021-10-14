35da4b2f5d313fdb1ed392dceb14fa3692817afd
axaysushir <akshay.sushir99@gmail.com>
2021-10-13 18:00:05 -0600

    APi in go

--- mods/main.go
+++ mods/main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type Course struct {
+	Name   string
+	Id     int
+	Author string
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
@@ -14,4 +20,18 @@ func main() {
 
 func HomeHandler() {
 	fmt.Println("Welcome Home")
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for _, course : range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found")
 }
