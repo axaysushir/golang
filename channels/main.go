c75ca9ec07ce3832b5ab1972b8d495eb5c1c6d04
axaysushir <akshay.sushir99@gmail.com>
2021-11-22 16:50:01 +0530

    Channel in golang

---
+++ channels/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	myCh := make(chan int, 2)
+	wg := &sync.WaitGroup{}
+
+	// fmt.Println(<-mych)
+	// myCh <- 5
+	wg.Add(2)
+
+	// Read only
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		val, isChanelOpen := <-myCh
+
+		fmt.Println(isChanelOpen)
+		fmt.Println(val)
+
+		wg.Done()
+
+	}(myCh, wg)
+
+	// Send only
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		myCh <- 0
+		close(myCh)
+
+		wg.Done()
+
+	}(myCh, wg)
+	wg.Wait()
+}
